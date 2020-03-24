34fe5e141c769c7b78aa081afbe473a98f3e00a6
Zifang98 <59437442+Zifang98@users.noreply.github.com>
2020-03-24 14:51:13 -0700

    update Jquery

---
+++ scripts/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+   $(".slide-section").click(function(e) {
+
+     var linkHref = $(this).attr('href');
+
+     $('html, body').animate({
+       scrollTop: $(linkHref).offset().top
+     }, 800);
+
+     e.preventDefault();
+   });
+
+});
